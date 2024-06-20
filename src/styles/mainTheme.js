b60cf6f726beffa08e5642c3f930aa1458faa9a3
Mela Maluenda <maluenda.mela@gmail.com>
2024-06-20 18:18:53 +0200

    Add global and mainTheme styles

---
+++ src/styles/mainTheme.js
@@ -0,0 +1,19 @@
+const mainTheme = {
+  colors: {
+    mainText: "#000",
+    greyText: "#b8",
+    lighter: "#E0C11E",
+  },
+  typography: {
+    mainFontFamily: "'Montserrat', sans-serif",
+    titleFontFamily: "'Playfair Display', serif",
+    superTitleSize: "2.5rem",
+    superSize: "2rem",
+    mainSize: "1.7rem",
+    titleSize: "1.25rem",
+    secondaryTitleSize: "1.15rem",
+    regularSize: "1rem",
+  },
+};
+
+export default mainTheme;
