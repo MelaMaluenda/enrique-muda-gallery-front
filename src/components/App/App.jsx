a2d53885b417de51507e595df8de5237371e3832
Mela Maluenda <maluenda.mela@gmail.com>
2024-06-20 17:20:24 +0200

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1> Enrique Muda Gallery </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    include: ["./src/**/*.test.{ts,tsx,js, jsx}"],
+    coverage: {
+      provider: "v8",
+      reporter: ["lcov", "text"],
+      all: true,
+      include: ["**/src/**/*.{ts,tsx,js, js}"],
+      exclude: [
+        "**/*.test.{ts,tsx,js, js}",
+        "**/types.js",
+        "**/*.d.js",
+        "**/src/main.jsx",
+      ],
+    },
+  },
+});
