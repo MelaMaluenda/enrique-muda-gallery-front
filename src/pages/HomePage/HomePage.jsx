c538da401095364e594f73985907d224c3a1166b
Mela Maluenda <maluenda.mela@gmail.com>
2024-06-27 20:56:16 +0200

    Add home and work page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import HomePageStyled from "./HomePageSyled";
+
+const HomePage = () => {
+  return (
+    <HomePageStyled>
+      <div className="photo" />
+    </HomePageStyled>
+  );
+};
+
+export default HomePage;
