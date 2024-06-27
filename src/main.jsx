eb51bbb8cf6271360c373e69af1872994c552fd6
Mela Maluenda <maluenda.mela@gmail.com>
2024-06-27 20:52:37 +0200

    Add i18next translations

--- src/main.jsx
+++ src/main.jsx
@@ -11,14 +11,33 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import mainTheme from "./styles/mainTheme";
 import GlobalStyle from "./styles/GlobalStyle";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import home_eng from "./translation/english/home.json";
+import home_esp from "./translation/español/home.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "eng",
+  resources: {
+    eng: {
+      home: home_eng,
+    },
+    esp: {
+      home: home_esp,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={mainTheme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <I18nextProvider i18n={i18next}>
+      <BrowserRouter>
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </I18nextProvider>
   </React.StrictMode>,
 );
