a9d20ad9b657306154843564011baa9a95ce4982
Mela Maluenda <maluenda.mela@gmail.com>
2024-06-27 20:43:06 +0200

    Add navigation and header components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import Navigation from "../Navigation/Navigation";
+import HeaderStyled from "./HeaderStyled";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
+const Header = () => {
+  const [t, i18n] = useTranslation("home");
+
+  return (
+    <HeaderStyled className="head">
+      <div className="head__left">
+        <Link aria-label="home" to="/home">
+          <h2>{t("navigation.name")}</h2>
+        </Link>
+      </div>
+      <div className="head__right">
+        <Navigation />
+        <button
+          className="button--lang"
+          onClick={() => i18n.changeLanguage("esp")}
+        >
+          ESP
+        </button>
+        <button
+          className="button--lang"
+          onClick={() => i18n.changeLanguage("eng")}
+        >
+          ENG
+        </button>
+      </div>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
