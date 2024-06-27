e352c75c9806e2898ccf3379cffd398e0d0946b8
Mela Maluenda <maluenda.mela@gmail.com>
2024-06-27 20:47:11 +0200

    Add bio page component

---
+++ src/pages/BioPage/BioPage.jsx
@@ -0,0 +1,30 @@
+import BioPageStyled from "./BioPageStyled";
+import Footer from "../../components/Footer/Footer";
+import { useTranslation } from "react-i18next";
+
+const BioPage = () => {
+  const [t] = useTranslation("home");
+
+  return (
+    <BioPageStyled>
+      <div className="bio-content">
+        <img
+          src="images/eqe.jpeg"
+          alt="Enrique Muda "
+          className="bio__image"
+          width={200}
+          height={200}
+        />
+        <div className="bio__text">
+          <p className="bio__text--p">{t("biography.p1")}</p>
+          <p className="bio__text--p">{t("biography.p2")}</p>
+          <p className="bio__text--p">{t("biography.p3")}</p>
+          <p className="bio__text--p">{t("biography.p4")}</p>
+        </div>
+      </div>
+      <Footer />
+    </BioPageStyled>
+  );
+};
+
+export default BioPage;
